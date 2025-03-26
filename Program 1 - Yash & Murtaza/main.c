dfda31421ba25cbb4ef25989bf2915f7e3873abe
GokuSSGodd <murtazaamjad0519@gmail.com>
2025-03-26 15:42:51 -0400

    Initial commit & adding Part 1 of Project

---
+++ Program 1 - Yash & Murtaza/main.c
@@ -0,0 +1,50 @@
+//Yash Desai & Murtaza Amjad
+//Program 1 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void usage();
+
+int main(int argc, char* argv[]){
+  FILE* file = fopen("P1_Yash.txt","a");
+  if(file == NULL){
+    printf("File not opened/ invalid file. Exiting");
+    return 1;
+  }
+  fprintf(file,"\n--------------------------------------------------");
+  printf("\n--------------------------------------------------");
+  fprintf(file,"\n command\t %s",argv[0]);
+  printf("\n command\t %s",argv[0]);
+  for(int j=1;j<argc;j++){
+    if(argv[j][0] == '-'){
+      fprintf(file,"\n switch\t");
+      printf("\n switch\t");
+      for(int k=1;k<strlen(argv[j]);k++){
+        if(argv[j][k]=='-') continue;
+        fprintf(file,"%c",argv[j][k]);
+        printf("%c",argv[j][k]);
+      }
+    }
+    else{
+      fprintf(file,"\n argument %s",argv[j]);
+      printf("\n argument %s",argv[j]);
+    }
+  }
+  printf("\n");
+  fclose(file);
+  return 0;
+}
+
+void usage(){
+  printf("Usage: option [-abRou] [-i filename] \n");
+  exit(1);
+}
+
+/* 
+Question 5 
+
+e. The output was sent to the file instead of to the console/command line. 
+f. It reads the names of the dummy 1,2, and 3 files individually as an argument. Since there are no log files the argument was *.log. 
+
+*/ 
